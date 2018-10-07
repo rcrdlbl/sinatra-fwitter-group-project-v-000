593e6602440422c0313635b80593ee664230cddc
Przemyslaw Dabrowski <bluh@outlook.com>
2018-10-07 10:47:30 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,14 @@
 class TweetsController < ApplicationController
-  
+  get '/tweets' do
+    if Helpers.logged_in?(session)
+      @user = User.find_by(id: session[:user_id])
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
+
 
 end
